062589f55ebda83e9e9a68aa87355202c6408a42
alexo12 <alex.r.otero12@gmail.com>
2022-05-07 22:41:52 -0500

    5/7 deployment

---
+++ build/precache-manifest.67e347702aa5d8488c83c89ab9827969.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ba7d8a1b65b4a09b697",
+    "url": "/movies-instaSearch/static/css/main.a57257c1.chunk.css"
+  },
+  {
+    "revision": "1ba7d8a1b65b4a09b697",
+    "url": "/movies-instaSearch/static/js/main.1ba7d8a1.chunk.js"
+  },
+  {
+    "revision": "39ad2adeb1187bca9a9a",
+    "url": "/movies-instaSearch/static/js/1.39ad2ade.chunk.js"
+  },
+  {
+    "revision": "aa4ec543b323cfb812ed",
+    "url": "/movies-instaSearch/static/js/runtime~main.aa4ec543.js"
+  },
+  {
+    "revision": "71dfdf62b6451170c3b89a34c1b81b93",
+    "url": "/movies-instaSearch/static/media/movieIcon-150x150.71dfdf62.png"
+  },
+  {
+    "revision": "4287edc94ba40754ec8f0d0ee1468ed7",
+    "url": "/movies-instaSearch/static/media/TOMMY___.4287edc9.TTF"
+  },
+  {
+    "revision": "4670e96c3406d72b64242ecfba473756",
+    "url": "/movies-instaSearch/static/media/trianglify-background3.4670e96c.svg"
+  },
+  {
+    "revision": "9de64e4f1e185a5ed07726ff768a7bbe",
+    "url": "/movies-instaSearch/index.html"
+  }
+];
